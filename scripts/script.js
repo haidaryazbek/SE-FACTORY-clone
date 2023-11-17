4b9e4c1cdb2aa16b48e56fd2fbc07ce78154aa4f
haidaryazbek <haidaryazbek66@gmail.com>
2023-11-17 09:07:07 +0200

    final commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
-console.log('test');
+
+const menu_section=document.getElementById("menu-section");
+const menu_toggler=document.getElementById("menu-toggler");
+
+menu_toggler.addEventListener("click",function(){
+    menu_section.classList.toggle("hidden");
+});
+
+
